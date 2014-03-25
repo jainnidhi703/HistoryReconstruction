7347002e46e7cf31e22c3eaf32b80176efdf6a9b
Jaydeep Solanki <jaydp17@gmail.com>
2014-03-25 23:27:13 +0530

    ExportDocument --documentation

--- src/ExportDocument.java
+++ src/ExportDocument.java
@@ -11,21 +11,43 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Exports results to Various file formats
+ */
 public class ExportDocument {
 
     private static final String HEADER = "Summary created using " + Globals.APP_NAME + " v[" + Globals.APP_VERSION + "]\n" +
             "============================================================\n\n";
 
+    /**
+     * Creates a proper title for the document, along with the query used
+     * to get the results
+     * @param query (query used to get the results)
+     * @return String (title)
+     */
     private static String getTitle(String query) {
         return "Query : [ "+ query + " ]\n------------------------------------------------------------\n\n";
     }
 
+    /**
+     * Generates a proper footer for the document,
+     * along with current date/time
+     * @return String (footer)
+     */
     private static String getFooter() {
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date date = new Date();
         return "\n\n\n- " + dateFormat.format(date);
     }
 
+
+    /**
+     * Exports the results to text file
+     * @param path Path to store the text file
+     * @param query Query used to get the results
+     * @param content What to write in the text file
+     * @throws IOException
+     */
     public static void toText(String path, String query, String content) throws IOException {
         final String title = getTitle(query);
         final String footer = getFooter();
@@ -35,6 +57,15 @@ public class ExportDocument {
         fw.close();
     }
 
+
+    /**
+     * Exports the results to PDF file
+     * @param path Path to store the text file
+     * @param query Query used to get the results
+     * @param content What to write in the text file
+     * @throws FileNotFoundException
+     * @throws DocumentException
+     */
     public static void toPDF(String path, String query, String content) throws FileNotFoundException, DocumentException {
         final String title = getTitle(query);
         final String footer = getFooter();
